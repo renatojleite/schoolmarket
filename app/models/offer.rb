382a95853fc193dd2324a9231246b87aa42209c4
Carlos Roberto <carlosbuanaoda@gmail.com>
2019-08-26 17:58:17 -0300

    Create offer categories

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,8 @@
 class Offer < ApplicationRecord
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
+  enum category: %i[first_grade second_grade third_grade fourth_grade fifth_grade
+                    sixth_grade seventh_grade eight_grade hoodies pants t_shirts shorts]
 
+  validates :category, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_200211) do
+ActiveRecord::Schema.define(version: 2019_08_26_203946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_200211) do
     t.bigint "seller_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category"
     t.index ["buyer_id"], name: "index_offers_on_buyer_id"
     t.index ["seller_id"], name: "index_offers_on_seller_id"
   end
