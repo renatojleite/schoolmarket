64d5f76b8c171910d5b0c87d6a0ef2940307d0de
Renato Nobrega <renatojleite@gmail.com>
2019-08-27 13:56:05 -0300

    schema-review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :offers
+  has_many :transactions
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :sales,
-           dependent: :destroy,
-           inverse_of: :seller,
-           class_name: "Offer",
-           foreign_key: "seller_id"
-  has_many :purchases,
-           inverse_of: :buyer,
-           class_name: "Offer",
-           foreign_key: "buyer_id"
 end
